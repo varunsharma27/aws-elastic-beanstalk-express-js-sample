836852a9e700850329540d410839d4d30f3c0ad5
Varun Sharma <contactvarun27@gmail.com>
2021-02-10 19:54:19 +0000

    Return HTML instead of text

--- app.js
+++ app.js
@@ -3,8 +3,33 @@ const app = express();
 const port = 8080;
 
 app.get('/:name?', (req, res) => {
+  res.set('Content-Type', 'text/html');
   const noun = req.params.name || 'World';
-  res.send(`Hello ${noun}!`);
+  res.send(Buffer.from((`
+    <html>
+      <head>
+        <style>
+          .body {
+            background-color: #8080802b;
+          }
+          .body:hover {
+            background-color: #80808038;
+          }
+          .text {
+            position: absolute;
+            text-align: center;
+            top: 50%;
+            width: 99%;
+          }
+        </style>
+      </head>
+      <body class="body">
+        <h1 class="text">
+          Hello ${noun}!
+        </h1>
+      </body>
+    </html>
+  `)));
 });
 
 app.listen(port);
