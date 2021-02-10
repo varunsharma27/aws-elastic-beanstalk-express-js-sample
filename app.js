f54b89babdb3ca3a565ee72232378f105fadb36c
Varun Sharma <contactvarun27@gmail.com>
2021-02-10 16:51:29 +0000

    Changed greeting

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/:name?', (req, res) => {
+  const noun = req.params.name || 'World';
+  res.send(`Hello ${noun}!`);
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
